44ebd512661c51adda37166625af357bfa15c254
Patrick Walters <patrickwalters@hotmail.com>
2020-10-07 00:41:53 -0400

    Worked on disabling submit dynamically

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 // Here's our javascript file
+var buttonSubmit = document.getElementById("btnSubmit");
+var clickSubmit = document.getElementsByClassName('click')[1];
+
+var noteField = document.getElementById("theNote");
+// The button is disabled on initial page load
+function disableSubmit() = {
+    document.getElementById("btnSubmit").disabled = true;
+}
+
+// listen for the note field to have a length > 0:
+noteField.addEventListener(length > 0, function(event) {
+    buttonSubmit.disabled = !buttonSubmit.disabled;
+});
+
