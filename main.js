ee98bb0efe87c5afa1dff12484acd0f1327bdd0c
Patrick Walters <patrickwalters@hotmail.com>
2020-10-15 02:00:10 -0400

    Value of textbox displayed from alert

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 // Here's our javascript file
-var buttonSubmit = document.getElementById("btnSubmit");
-var clickSubmit = document.getElementsByClassName('click')[1];
-
-var noteField = document.getElementById("theNote");
 // The button is disabled on initial page load
 function disableSubmit() {
     document.getElementById("btnSubmit").disabled = true;
 }
 
 function myFunction() {
-    alert("The field had: " + noteField);
-  }
+    alert(theNote.value);
+}
 
 // listen for the note field to have a length > 0:
 noteField.addEventListener("noteField", myFunction);
